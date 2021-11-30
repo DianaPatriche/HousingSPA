fd2fcc5bbf089608ab021f32c1a5bc524f0c64b5
Diana <dianapatriche.21@gmail.com>
2021-11-30 10:51:14 +0200

    Create property list + add CSS animation to property card

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -7,6 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+  properties: Array<any> = [
+    {
+      "Id":1,
+      "Name":"Birla House",
+      "Type":"House",
+      "Price":12000
+    },
+    {
+      "Id":2,
+      "Name":"Gold Apartment",
+      "Type":"Apartment",
+      "Price":27800
+    },
+    {
+      "Id":3,
+      "Name":"Diamond House",
+      "Type":"House",
+      "Price":30000
+    },
+    {
+      "Id":4,
+      "Name":"Velvet Apartment",
+      "Type":"Apartment",
+      "Price":12000
+    },
+    {
+      "Id":5,
+      "Name":"Tower House",
+      "Type":"House",
+      "Price":17800
+    },
+    {
+      "Id":6,
+      "Name":"Green House",
+      "Type":"House",
+      "Price":20000
+    },
+    {
+      "Id":7,
+      "Name":"Alfa House",
+      "Type":"House",
+      "Price":15500
+    }
+]
+
   constructor() { }
 
   ngOnInit(): void {
